8cae589d8e2c31d235c32405ee3d1f46039bff62
Matt Lahti <matt.lahti71@gmail.com>
2025-12-16 11:34:54 -0600

    Show the location stats in the popup. Add more clear buttons and disable them if there is nothing to clear.

--- popup/popup.js
+++ popup/popup.js
@@ -1,7 +1,9 @@
 import {
-    getAllTotalHideCounts,
-    getAllSessionHideCounts,
+    getAllChannelHideCounts,
+    getAllLocationHideCounts,
     clearAllHideCounts,
+    clearChannelHideCount,
+    clearLocationHideCount,
 } from '../src/count-storage.js';
 import {
     getEnabledLocations,
@@ -14,13 +16,17 @@ import {
     LOCATIONS,
 } from '../src/site-location.js';
 
-const statsList = document.getElementById('stats-list');
-const clearButton = document.getElementById('stats-clear-button');
+const channelStatsList = document.getElementById('channel-stats-list');
+const locationStatsList = document.getElementById('location-stats-list');
+const channelStatsClearButton = document.getElementById('channel-stats-clear-button');
+const locationStatsClearButton = document.getElementById('location-stats-clear-button');
+const allStatsClearButton = document.getElementById('all-stats-clear-button');
 const locations = document.getElementById('settings-locations');
 const statsEnabledElement = document.getElementById('settings-stats-enabled');
 
-const EMPTY_STATS_TEXT = 'No videos hidden yet.';
-const STATS_DISABLED_TEXT = 'Statistics are disabled - to track and view, enable them in the settings tab.';
+const TEXT_STATS_ERROR = 'Failed to load statistics.';
+const EMPTY_STATS_TEXT = 'No data to display.';
+const STATS_DISABLED_TEXT = 'Statistics are disabled. To track and view them, enable statistics in the settings tab.';
 
 const TAB_STATISTICS = 'stats-tab';
 const TAB_SETTINGS = 'settings-tab';
@@ -37,7 +43,8 @@ const TAB_SECTIONS = {
 };
 
 let activeTab = TAB_STATISTICS;
-let hasStats = false;
+let hasChannelStats = false;
+let hasLocationStats = false;
 
 const updateTabDom = () => {
     Object.values(TAB_BUTTONS).forEach(button => button.classList.remove('tab-active'));
@@ -51,31 +58,44 @@ const onTabClick = e => {
     updateTabDom();
 };
 
-const renderStats = (totalCounts, sessionCounts) => {
-    statsList.textContent = '';
-    const entries = Object.entries(totalCounts);
+const renderChannelStats = totalCounts => {
+    channelStatsList.textContent = '';
 
-    if (entries.length === 0) {
-        hasStats = false;
-        statsList.textContent = EMPTY_STATS_TEXT;
+    if (!hasChannelStats) {
+        locationStatsList.textContent = EMPTY_STATS_TEXT;
 
         return;
     }
 
-    hasStats = true;
+    const entries = Object.entries(totalCounts);
     const sortedEntries = entries.sort((a, b) => b[1] - a[1]);
 
     for (const [channel, count] of sortedEntries) {
         const li = document.createElement('li');
-        let text = `${channel}: ${count}`;
+        li.textContent = `${channel}: ${count}`;
+        channelStatsList.appendChild(li);
+    }
+};
 
-        const sessionCount = sessionCounts[channel];
-        if (sessionCount > 0) {
-            text += ` (${sessionCount} this session)`;
-        }
+const renderLocationStats = locationCounts => {
+    locationStatsList.textContent = '';
 
-        li.textContent = text;
-        statsList.appendChild(li);
+    if (!hasLocationStats) {
+        locationStatsList.textContent = EMPTY_STATS_TEXT;
+
+        return;
+    }
+
+    const entries = Object.entries(locationCounts);
+
+    // todo: at this point, should I fill in the other known locations and default them to 0?
+
+    const sortedEntries = entries.sort((a, b) => b[1] - a[1]);
+
+    for (const [location, count] of sortedEntries) {
+        const li = document.createElement('li');
+        li.textContent = `${getLocationPrettyName(location)}: ${count}`;
+        locationStatsList.appendChild(li);
     }
 };
 
@@ -83,37 +103,34 @@ const fetchAndRenderStats = async () => {
     const statsEnabled = await areStatsEnabled();
 
     if (!statsEnabled) {
-        statsList.textContent = STATS_DISABLED_TEXT;
+        channelStatsList.textContent = STATS_DISABLED_TEXT;
 
         return;
     }
 
     try {
-        const [totalCounts, sessionCounts] = await Promise.all([
-            getAllTotalHideCounts(),
-            getAllSessionHideCounts()
+        const [
+            channelHideCounts,
+            locationHideCounts,
+        ] = await Promise.all([
+            getAllChannelHideCounts(),
+            getAllLocationHideCounts(),
         ]);
 
-        renderStats(totalCounts, sessionCounts);
+        hasChannelStats = !!Object.keys(channelHideCounts).length;
+        hasLocationStats = !!Object.keys(locationHideCounts).length;
+        updateClearButtonVisibilities();
+        renderChannelStats(channelHideCounts);
+        renderLocationStats(locationHideCounts);
     } catch (err) {
         console.error('Failed to load hide‑count stats:', err);
-        statsList.textContent = 'Failed to load statistics.';
+        channelStatsList.textContent = TEXT_STATS_ERROR;
+        locationStatsList.textContent = TEXT_STATS_ERROR;
     }
 };
 
 const checkStatsEnabled = async () => {
     statsEnabledElement.querySelector('input[type="checkbox"]').checked = await areStatsEnabled();
-};
-
-const updateClearButtonVisibility = async () => {
-    const statsEnabled = await areStatsEnabled();
-    clearButton.style.display = hasStats && statsEnabled ? 'block' : 'none';
-};
-
-const clearStats = async () => {
-    await clearAllHideCounts();
-    await fetchAndRenderStats();
-    await updateClearButtonVisibility();
 };
 
 const getEnabledLocationsFromCheckboxes = () =>
@@ -136,14 +153,51 @@ const onEnabledLocationsChange = async e => {
 
 const onStatsEnabledChange = async e => {
     await updateStatsEnabled(e.target.checked);
-    await updateClearButtonVisibility();
+    await fetchAndRenderStats();
+};
+
+const enableOrDisableButton = (button, enabled) => {
+    const enabledColor = '#ff7878';
+    const disabledColor = '#999999';
+
+    if (enabled) {
+        button.style.color = enabledColor;
+        button.style.cursor = 'pointer';
+        button.removeAttribute('disabled');
+    } else {
+        button.style.color = disabledColor;
+        button.style.cursor = 'not-allowed';
+        button.setAttribute('disabled', '');
+    }
+}
+
+const updateClearButtonVisibilities = () => {
+    enableOrDisableButton(channelStatsClearButton, hasChannelStats);
+    enableOrDisableButton(locationStatsClearButton, hasLocationStats);
+    enableOrDisableButton(allStatsClearButton, hasChannelStats || hasLocationStats);
+};
+
+const clearChannelStats = async () => {
+    await clearChannelHideCount();
+    await fetchAndRenderStats();
+};
+
+const clearLocationStats = async () => {
+    await clearLocationHideCount();
+    await fetchAndRenderStats();
+};
+
+const clearAllStats = async () => {
+    await clearAllHideCounts();
     await fetchAndRenderStats();
 };
 
 const bindEventListeners = () => {
     locations.addEventListener('change', onEnabledLocationsChange);
     statsEnabledElement.addEventListener('change', onStatsEnabledChange);
-    clearButton.addEventListener('click', clearStats);
+    channelStatsClearButton.addEventListener('click', clearChannelStats);
+    locationStatsClearButton.addEventListener('click', clearLocationStats);
+    allStatsClearButton.addEventListener('click', clearAllStats);
     Object.values(TAB_BUTTONS).forEach(tabButton => tabButton.addEventListener('click', onTabClick));
 };
 
@@ -170,7 +224,6 @@ const init = async () => {
     await populateLocations();
     await checkStatsEnabled();
     await fetchAndRenderStats();
-    await updateClearButtonVisibility();
     bindEventListeners();
     updateTabDom();
 };
