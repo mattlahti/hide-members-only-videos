434fd95f909089e0632a035f4c2501070042dfc5
Matt Lahti <matt.lahti71@gmail.com>
2025-12-22 07:16:34 -0600

    Refactor debug logs to support multiple severities

--- src/logger.js
+++ src/logger.js
@@ -1,13 +1,31 @@
 import { areDebugLogsEnabled } from './settings-storage.js';
 
-const debugLog = async (...args) => {
+const LOG_TYPE_DEBUG = 1;
+const LOG_TYPE_WARNING = 2;
+const LOG_TYPE_ERROR = 4;
+
+const LOG_TYPE_METHOD_MAP = {
+    [LOG_TYPE_DEBUG]: console.log,
+    [LOG_TYPE_WARNING]: console.warn,
+    [LOG_TYPE_ERROR]: console.error,
+};
+
+const log = async (type, ...args) => {
     if (!await areDebugLogsEnabled()) {
         return;
     }
 
-    console.log('[Hide Members Only Videos]', ...args);
+    return LOG_TYPE_METHOD_MAP[type]('[Hide Members Only Videos]', ...args);
 };
+
+const debugLog = async (...args) => await log(LOG_TYPE_DEBUG, ...args);
+
+const debugWarningLog = async (...args) => await log(LOG_TYPE_WARNING, ...args);
+
+const debugErrorLog = async (...args) => await log(LOG_TYPE_ERROR, ...args);
 
 export {
     debugLog,
+    debugWarningLog,
+    debugErrorLog,
 };
